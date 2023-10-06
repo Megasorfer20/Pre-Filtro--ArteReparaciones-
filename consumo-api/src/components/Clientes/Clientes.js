f66973489741c45d1baa2a8cee00c58fa9fc8779
Esteban Ruiz <edrp.campus15@gmail.com>
2023-10-06 12:34:53 -0500

    FormulariosClientes

--- consumo-api/src/components/Clientes/Clientes.js
+++ consumo-api/src/components/Clientes/Clientes.js
@@ -1,12 +1,20 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Table, Button } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Table, Button, Form, Input } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import "./Clientes.css";
 import ListadoClientes from "./ListaClientes";
+import axios from "axios";
 
 export default function Clientes() {
   const [productosApiData, setProductosApiData] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [formData, setFormData] = useState({
+    Nombre: "",
+    Apellido: "",
+    Direccion: "",
+    Telefono: "",
+    Email: "",
+  });
 
   useEffect(() => {
     axios.get("http://localhost:5000/clientes").then((response) => {
@@ -25,12 +33,106 @@ export default function Clientes() {
     localStorage.setItem("COMPRACLIENTE", Email);
   };
 
+  const handleFormSubmit = () => {
+    // Envía los datos del formulario al servidor usando axios o fetch
+    axios.post("http://localhost:5000/clientes", formData).then((response) => {
+      console.log("Cliente creado con éxito:", response.data);
+      // Actualiza la lista de clientes después de agregar uno nuevo
+      setProductosApiData([...productosApiData, response.data]);
+      // Restablece el estado del formulario
+      setFormData({
+        Nombre: "",
+        Apellido: "",
+        Direccion: "",
+        Telefono: "",
+        Email: "",
+      });
+      // Oculta el formulario
+      setShowForm(false);
+    });
+  };
+
+  const handleCancel = () => {
+    // Restablece el estado del formulario
+    setFormData({
+      Nombre: "",
+      Apellido: "",
+      Direccion: "",
+      Telefono: "",
+      Email: "",
+    });
+    // Oculta el formulario
+    setShowForm(false);
+  };
+
   return (
     <React.Fragment>
-      <h1>Informacion de Productos</h1>
-      <Link to="/productos/add">
-        <Button>Nuevo Cliente</Button>
-      </Link>
+      <h1>Información de Productos</h1>
+      <Button onClick={() => setShowForm(true)}>Nuevo Cliente</Button>
+      {showForm && (
+        <div>
+          <h2>Crear Nuevo Cliente</h2>
+          <Form className="form" onSubmit={handleFormSubmit}>
+            <Form.Field>
+              <label>Nombre</label>
+              <Input
+                type="text"
+                placeholder="Nombre"
+                value={formData.Nombre}
+                onChange={(e) =>
+                  setFormData({ ...formData, Nombre: e.target.value })
+                }
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Apellido</label>
+              <Input
+                type="text"
+                placeholder="Apellido"
+                value={formData.Apellido}
+                onChange={(e) =>
+                  setFormData({ ...formData, Apellido: e.target.value })
+                }
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Dirección</label>
+              <Input
+                type="text"
+                placeholder="Dirección"
+                value={formData.Direccion}
+                onChange={(e) =>
+                  setFormData({ ...formData, Direccion: e.target.value })
+                }
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Teléfono</label>
+              <Input
+  type="tel"
+  placeholder="Teléfono"
+  value={formData.Telefono}
+  onChange={(e) =>
+    setFormData({ ...formData, Telefono: e.target.value })
+  }
+/>
+            </Form.Field>
+            <Form.Field>
+              <label>Email</label>
+              <Input
+                type="email"
+                placeholder="Email"
+                value={formData.Email}
+                onChange={(e) =>
+                  setFormData({ ...formData, Email: e.target.value })
+                }
+              />
+            </Form.Field>
+            <Button type="submit">Guardar</Button>
+            <Button type="button" onClick={handleCancel}>Cancelar</Button> {/* Agrega el botón de cancelar */}
+          </Form>
+        </div>
+      )}
       {productosApiData.map((element) => {
         return <ListadoClientes key={element._id} data={element} />;
       })}
--- consumo-api/src/components/Compras/Compras.js
+++ consumo-api/src/components/Compras/Compras.js
@@ -1,15 +1,24 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Table, Button } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Table, Button, Form, Input, Dropdown } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import "./Compras.css";
 import ListadoCompras from "./ListaCompras";
+import axios from "axios";
 
 export default function Compras() {
   const [comprasApiData, setcomprasApiData] = useState([]);
   const [empleadosApiData, setEmpleadosApiData] = useState([]);
   const [proveedoresApiData, setproveedoresApiData] = useState([]);
   const [InventariosApiData, setInventariosApiData] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [formData, setFormData] = useState({
+    FechaCompra: "",
+    Proveedor: "",
+    Elementos: [],
+    Empleado: "",
+    FechaEntrega: "",
+    TotalPagar: "",
+  });
 
   useEffect(() => {
     axios.get("http://localhost:5000/compras").then((response) => {
@@ -34,23 +43,161 @@ export default function Compras() {
   }, []);
 
   const setComprasData = (data) => {
-    let {_id,FechaCompra,Proveedor,Elementos,Empleado,FechaEntrega,TotalPagar,
-    } = data;
+    let { _id, FechaCompra, Proveedor, Elementos, Empleado, FechaEntrega, TotalPagar } = data;
     localStorage.setItem("COMPRAID", _id);
     localStorage.setItem("FECHACOMPRA", FechaCompra);
-    localStorage.setItem("PROVEEDORCOMPRA",Proveedor);
+    localStorage.setItem("PROVEEDORCOMPRA", Proveedor);
     localStorage.setItem("ELEMENTOSCOMPRA", Elementos);
     localStorage.setItem("EMPLEADOCOMPRAS", Empleado);
     localStorage.setItem("FECHAENTREGACOMPRAS", FechaEntrega);
     localStorage.setItem("TOTALPAGARCOMPRAS", TotalPagar);
   };
 
+  const handleFormSubmit = () => {
+    // Envía los datos del formulario al servidor usando axios o fetch
+    axios.post("http://localhost:5000/compras", formData).then((response) => {
+      console.log("Compra creada con éxito:", response.data);
+      // Actualiza la lista de compras después de agregar una nueva
+      setcomprasApiData([...comprasApiData, response.data]);
+      // Restablece el estado del formulario
+      setFormData({
+        FechaCompra: "",
+        Proveedor: "",
+        Elementos: [],
+        Empleado: "",
+        FechaEntrega: "",
+        TotalPagar: "",
+      });
+      // Oculta el formulario
+      setShowForm(false);
+    });
+  };
+
+  const handleCancel = () => {
+    // Restablece el estado del formulario
+    setFormData({
+      FechaCompra: "",
+      Proveedor: "",
+      Elementos: [],
+      Empleado: "",
+      FechaEntrega: "",
+      TotalPagar: "",
+    });
+    // Oculta el formulario
+    setShowForm(false);
+  };
+
+  const handleProveedorChange = (e, { value }) => {
+    setFormData({ ...formData, Proveedor: value });
+  };
+
+  const handleEmpleadoChange = (e, { value }) => {
+    setFormData({ ...formData, Empleado: value });
+  };
+
+  const handleInventarioChange = (e, { value }) => {
+    setFormData({ ...formData, Elementos: value });
+  };
+
   return (
     <React.Fragment>
       <h1>Informacion de Compras</h1>
-      <Link to="/comprass/add">
-        <Button>Nueva Compra</Button>
-      </Link>
+      <Button onClick={() => setShowForm(true)}>Nueva Compra</Button>
+      {showForm && (
+        <div>
+          <h2>Crear Nueva Compra</h2>
+          <Form className="form" onSubmit={handleFormSubmit}>
+            <Form.Field>
+              <label>Fecha de Compra</label>
+              <Input
+                type="date"
+                placeholder="Fecha de Compra"
+                value={formData.FechaCompra}
+                onChange={(e) =>
+                  setFormData({ ...formData, FechaCompra: e.target.value })
+                }
+              />
+            </Form.Field>
+            <Form.Field>
+  <label>Proveedor</label>
+  <select
+    value={formData.Proveedor}
+    onChange={(e) =>
+      setFormData({ ...formData, Proveedor: e.target.value })
+    }
+  >
+    <option value="">Selecciona un proveedor</option>
+    {proveedoresApiData.map((proveedor) => (
+      <option key={proveedor._id} value={proveedor._id}>
+        {proveedor.Nombre}
+      </option>
+    ))}
+  </select>
+</Form.Field>
+
+            <Form.Field>
+              <label>Elementos</label>
+              <Dropdown
+                fluid
+                multiple
+                search
+                selection
+                options={InventariosApiData.map((inventario) => ({
+                  key: inventario._id,
+                  text: inventario.Nombre,
+                  value: inventario._id,
+                }))}
+                placeholder="Selecciona elementos"
+                value={formData.Elementos}
+                onChange={handleInventarioChange}
+              />
+            </Form.Field>
+            <Form.Field>
+  <label>Empleado</label>
+  <select
+    value={formData.Empleado}
+    onChange={(e) =>
+      setFormData({ ...formData, Empleado: e.target.value })
+    }
+  >
+    <option value="">Selecciona un empleado</option>
+    {empleadosApiData.map((empleado) => (
+      <option key={empleado._id} value={empleado._id}>
+        {`${empleado.Nombre} ${empleado.Apellido}`}
+      </option>
+    ))}
+  </select>
+</Form.Field>
+
+            <Form.Field>
+              <label>Fecha de Entrega</label>
+              <Input
+                type="date"
+                placeholder="Fecha de Entrega"
+                value={formData.FechaEntrega}
+                onChange={(e) =>
+                  setFormData({ ...formData, FechaEntrega: e.target.value })
+                }
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Total a Pagar</label>
+              <Input
+                type="number"
+                placeholder="Total a Pagar"
+                value={formData.TotalPagar}
+                onChange={(e) =>
+                  setFormData({ ...formData, TotalPagar: e.target.value })
+                }
+              />
+            </Form.Field>
+            <Button type="submit">Guardar</Button>
+            <Button type="button" onClick={handleCancel}>
+              Cancelar
+            </Button>
+          </Form>
+        </div>
+      )}
       {comprasApiData.map((element) => {
         const empleadoRelacion = empleadosApiData.find(
           (el) => el._id === element.Empleado
@@ -59,14 +206,14 @@ export default function Compras() {
         const proveedorRelacion = proveedoresApiData.find(
           (el) => el._id === element.Proveedor
         );
-        
-        const inventariosRElacion = element.Elementos.map(elem => {
-          return InventariosApiData.find(
-            el => el._id === elem.ProductoComprado
-          )
-        })
 
-        console.log(inventariosRElacion);
+        const inventariosRelacion = element.Elementos.map((elem) => {
+          return InventariosApiData.find(
+            (el) => el._id === elem.ProductoComprado
+          );
+        });
+
+        console.log(inventariosRelacion);
 
         return (
           <ListadoCompras
@@ -74,7 +221,7 @@ export default function Compras() {
             data={element}
             empleado={empleadoRelacion}
             proveedor={proveedorRelacion}
-            inventarios={inventariosRElacion}
+            inventarios={inventariosRelacion}
           />
         );
       })}
